b370aa3e97f407634f0ab67c7d6760c6941d30c8
huseyincamci <hcamci.ce@gmail.com>
2021-04-10 13:48:03 +0300

    yazılım geliştirici yetiştirme kampı 9. ders sonu

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -4,6 +4,7 @@ using Entities.Concrete;
 using System.Linq;
 using System.Linq.Expressions;
 using System;
+using Entities.DTOs;
 
 namespace DataAccess.Concrete.InMemory
 {
@@ -49,6 +50,11 @@ namespace DataAccess.Concrete.InMemory
         public List<Product> GetAllByCategory(int categoryId)
         {
             return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
         }
 
         public void Update(Product product)
