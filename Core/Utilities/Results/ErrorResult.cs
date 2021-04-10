3b8cf8b70a8f6bd5c44a9eb065fd2119208a684c
huseyincamci <hcamci.ce@gmail.com>
2021-04-10 16:06:16 +0300

    yazılım geliştirici yetiştirme kampı 10. ders sonu

---
+++ Core/Utilities/Results/ErrorResult.cs
@@ -0,0 +1,14 @@
+namespace Core.Utilities.Results
+{
+    public class ErrorResult : Result
+    {
+        public ErrorResult() : base(false)
+        {
+        }
+
+        public ErrorResult(string message) : base(false, message)
+        {
+
+        }
+    }
+}
