fbcc487de665356d8ad010222e3e02c3ad733e0a
huseyincamci <hcamci.ce@gmail.com>
2021-04-10 21:28:08 +0300

    yazılım geliştirici yetiştirme kampı 12. ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -6,6 +6,10 @@ using Entities.DTOs;
 using Core.Utilities.Results;
 using Business.Constants;
 using System;
+using FluentValidation;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Aspects.Autofac.Validation;
 
 namespace Business.Concrete
 {
@@ -18,12 +22,9 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,5 +9,6 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductsListed = "Ürünler listelendi";
+        public static string UnitPriceInvalid = "";
     }
 }
